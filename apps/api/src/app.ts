e5128b083cd7c24428eb9637bf2d444e4abe3f28
Albin N J <albinnj0214@gmail.com>
2026-04-16 01:20:11 +0530

    add cicd for docker hub

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -7,7 +7,7 @@ const app: Application = express();
 // Middleware
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   }),
